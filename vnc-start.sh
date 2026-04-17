f3450c8d444decfd3029292bfe9e255f1a8cb1f9
adam2893 <167422559+adam2893@users.noreply.github.com>
2026-04-17 13:52:21 +0700

    Delete vnc-start.sh

--- vnc-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export XDG_RUNTIME_DIR="/run/user/1000"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-export DISPLAY=:1
-exec vncserver :1 -geometry "${VNC_GEOMETRY:-1920x1080}" -depth "${VNC_DEPTH:-24}" -localhost no -rfbauth /home/cachyos/.vnc/passwd
